45010767a26c7a1434adc14604b706d3cb6252da
Max Morgenbesser <maxwellmorgenbesser@gmail.com>
2022-08-01 17:04:03 -0400

    learning html from BocaCode

--- students.js
+++ students.js
@@ -6,9 +6,24 @@ const port = 3001
 app.use(cors())
 app.use(express.json())
 
-app.get("/", (req,res)=>{
-    res.send("Hello World")
+
+let students = ["bob","samantha","lily","grace","jim"]
+
+app.get("/",(req,res)=> {
+
+    let output = "<html><body><ul>" // starts
+    
+    //output = output + 'hello there html' //middle
+    for(let i = 0; i < students.length;i++) {
+        output = output + '<li><input type="checkbox">' + students[i] + '</li>'
+    }
+
+    output = output + "</ul></body></html>" //end
+
+    res.send(output)
 })
+
+
 app.listen(port,()=>
 {console.log("Now listening on ", port)})
 
